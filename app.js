123b22a4786d347e7f508295183cdd66d10bea78
Ivan Vera <ivandejesusvera@gmail.com>
2024-03-11 16:23:06 -0500

    Mejorada la estructura y el CSS, se agregó la sección de JavaScript para el funcionamiento de los botones, se incluyó un reseteo de CSS para mantenerlo limpio. Proyecto listo

---
+++ app.js
@@ -0,0 +1,55 @@
+//ocultar boton
+document.getElementById("btn-copiar").style.display = "none";
+
+function encriptar(){
+    var texto = document.getElementById("caja-texto").value;
+    var reemplazar = {
+        "e":"enter", 
+        "i":"imes", 
+        "a":"ai", 
+        "o":"ober", 
+        "u":"ufat"
+    };
+
+    texto = texto.replace(/[eiaou]/g, function(match){
+        return reemplazar[match];
+    });
+
+    document.getElementById("txt-resultado").textContent = texto;
+    document.querySelector('.mensaje').style.backgroundImage = 'none';
+    document.getElementById("caja-ocular").style.display = "none";
+    document.getElementById("btn-copiar").style.display = "block";
+};
+
+function desencriptar(){
+    var textoEncriptado = document.getElementById("caja-texto").value;
+    var reemplazar = {
+        "enter":"e", 
+        "imes":"i", 
+        "ai":"a", 
+        "ober":"o", 
+        "ufat":"u"
+    };
+
+    textoOriginal = textoEncriptado.replace(/enter|imes|ai|ober|ufat/g, function(match){
+        return reemplazar[match];
+    });
+
+    document.getElementById("txt-resultado").textContent = textoOriginal;
+    document.querySelector('.mensaje').style.backgroundImage = 'none';
+    document.getElementById("caja-ocular").style.display = "none";
+    document.getElementById("btn-copiar").style.display = "block";
+};
+
+function copiartxt(){
+    let texto = document.getElementById("txt-resultado").textContent;
+    const copiarConenido = async () => {
+        try {
+            await navigator.clipboard.writeText(texto);
+            console.log('contenido copiado');
+        } catch (err) {
+            console.error("error al copiar:", err);
+        }
+    }
+    copiarConenido();
+}
